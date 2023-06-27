ac12afc7e3c554912aadb885df0cc65cb69248de
Lisandro Guerra <lisandro.digital@gmail.com>
2023-06-27 14:30:05 +0100

    styles folder

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+    COLORS: {
+      BACKGROUND_900: "#262529",
+      BACKGROUND_800: "#1C1B1E",
+      BACKGROUND_700: "#312E38",
+      BACKGROUND_600: "#3E3B47",
+  
+      BLACK: '#0D0C0F',
+      WHITE: "#F4EDE8",
+      ICE: "#E5E5E5",
+      ROSE: "#FF859B",
+      ROSE5PC: "rgba(255, 133, 155, 0.05)",
+  
+      GRAY_100: "#CAC4CF",
+      GRAY_200: "#999591",
+      GRAY_300: "#948F99",
+  
+      RED: "#FF002E"
+    }
+}
